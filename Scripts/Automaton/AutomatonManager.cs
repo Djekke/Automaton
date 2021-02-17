6b80db3d61254f017d57602f2acfc0e5c89b633b
djekke <djekke@gmail.com>
2021-02-17 14:34:34 +0300

    0.4.0 (Move version up for new release)

--- Scripts/Automaton/AutomatonManager.cs
+++ Scripts/Automaton/AutomatonManager.cs
@@ -32,7 +32,7 @@
 
         public const string ModId = "Automaton";
 
-        public static Version CurrentVersion => new Version("0.3.6");
+        public static Version CurrentVersion => new Version("0.4.0");
 
         public const string RssFeed = //"http://github.com/Djekke/Automaton/releases.atom";
             "https://feedmix.novaclic.com/atom2rss.php?source=https%3A%2F%2Fgithub.com%2FDjekke%2FAutomaton%2Freleases.atom";
